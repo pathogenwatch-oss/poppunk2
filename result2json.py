b5fd7a416608198193e187c679e7f7e0bb91947c
Anthony Underwood <anthony.underwood@cgps.group>
2021-10-20 13:19:35 +0000

    catch empty results and report NAs as "Not assigned"
    Former-commit-id: 6c1c2e1f6753ba4ff046e89c4e3cb8ace20046c2

--- result2json.py
+++ result2json.py
@@ -8,9 +8,16 @@ result_file = "result/result_external_clusters.csv"
 if os.path.exists(result_file):
     with open(result_file) as result:
         reader = csv.DictReader(result)
-        result_info = next(reader)
-    # convert to JSON
-    print(json.dumps({'strain' : result_info['GPSC']}))
+        try:
+            result_info = next(reader)
+            # convert to JSON
+            if result_info['GPSC'] == "NA":
+                print(json.dumps({'strain' : "Not assigned"}))
+            else:
+                print(json.dumps({'strain' : result_info['GPSC']}))
+        except StopIteration:
+            print(json.dumps({'strain' : "Not assigned"}))
+
 else:
     print("Result file not found")
     sys.exit(1)
