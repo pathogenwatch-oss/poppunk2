496f54a21ad674374aa6f9e200c0ca5fd2b7150e
CorinYeatsCGPS <107122521+CorinYeatsCGPS@users.noreply.github.com>
2024-05-02 15:45:10 +0100

    Added the Vibrio library and added more generic support for other schemes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -eu -o pipefail
 
+LIBRARY=${1}
+LIBRARY_DIR=${LIBRARY}_db
+METADATA=${LIBRARY_DIR}/metadata.json
+
+OPTS=""
+RESULT_DIR="result"
+RESULT="${RESULT_DIR}/result_clusters.csv"
+KEY="Cluster"
+
+if [ -f ${LIBRARY}_db/${LIBRARY}_clusters.csv ]; then
+    OPTS="--external-clustering ${LIBRARY_DIR}/${LIBRARY}_clusters.csv"
+    RESULT="${RESULT_DIR}/result_external_clusters.csv"
+    KEY=${LIBRARY^^}
+fi
+
 cat - > sequence.fas
 
-conda run -n poppunk poppunk_assign --db /ref --query sample.txt --external-clustering gpsc_clusters.csv --output result > /dev/null 2> /dev/null
+conda run -n poppunk poppunk_assign --db /${LIBRARY_DIR} --query sample.txt ${OPTS} --output result > /dev/null 2>&1
 
-python result2json.py
+python result2json.py ${RESULT} ${METADATA} ${KEY}
--- result2json.py
+++ result2json.py
@@ -1,9 +1,20 @@
 import csv
 import json
-import sys
 import os
+import sys
 
-result_file = "result/result_external_clusters.csv"
+# "result/result_external_clusters.csv"
+result_file = sys.argv[1]
+metadata_file = sys.argv[2]
+key = 'Cluster' if len(sys.argv) == 3 else sys.argv[3]
+
+# Read metadata file
+if os.path.exists(metadata_file):
+    with open(metadata_file) as metadata:
+        metadata = json.load(metadata)
+else:
+    exit("Metadata file not found")
+
 # read in lineage report
 if os.path.exists(result_file):
     with open(result_file) as result:
@@ -11,12 +22,12 @@ if os.path.exists(result_file):
         try:
             result_info = next(reader)
             # convert to JSON
-            if result_info['GPSC'] == "NA":
-                print(json.dumps({'strain' : "Not assigned"}))
+            if result_info[key] == "NA":
+                print(json.dumps(metadata | {'strain': "Not assigned"}))
             else:
-                print(json.dumps({'strain' : result_info['GPSC']}))
+                print(json.dumps(metadata | {'strain': result_info[key]}))
         except StopIteration:
-            print(json.dumps({'strain' : "Not assigned"}))
+            print(json.dumps(metadata | {'strain': "Not assigned"}))
 
 else:
     print("Result file not found")
